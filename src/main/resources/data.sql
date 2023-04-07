82751659bf45e679036f041a99656c71a5ee663e
David <David@DESKTOP-721V7M3>
2023-04-07 16:49:25 +0200

    Segundo Commit. He editado el archivo data.sql para introducir 2 artículos y tres opiniones

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
-/**insert into articulo (id,titulo, autor, fecha_publicacion, revista_Publicacion) values (1,'Prueba2', 'David', '1980-01-01', 'DAM');*/
-insert into articulo (id,titulo, autor, fecha_publicacion, revista_Publicacion) values (2,'Prueba3', 'David', '2023-04-07', 'DAM');
+insert into articulo (id,titulo, autor, fecha_publicacion, revista_Publicacion) values (1,'Prueba1', 'David', '2023-04-20', 'DAM');
+insert into articulo (id,titulo, autor, fecha_publicacion, revista_Publicacion) values (2,'Prueba2', 'David', '2023-04-07', 'DAM');
 
 
-/*insert into opinion (id,nombre, valoracion, fecha_valoracion, razon,articulo_id) values (1,'Alejandro',10, '1980-04-01','funciona',1);*/
+insert into opinion (id,nombre, valoracion, fecha_valoracion, razon,articulo_id) values (1,'Alejandro',10, '2023-04-20','funciona',1);
+insert into opinion (id,nombre, valoracion, fecha_valoracion, razon,articulo_id) values (2,'Alex',10, '2023-04-20','funciona',2);
+insert into opinion (id,nombre, valoracion, fecha_valoracion, razon,articulo_id) values (3,'Aless',10, '2023-04-20','funciona',2);
